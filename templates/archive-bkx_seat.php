c3d58357cbee7d20c0142224521b338972c941e8
Divyang Parekh <dparekh4@gmail.com>
2020-04-08 12:34:37 +0530

    Bredcrumb Set up
    Bredcrumb set for Archive and Single post page

--- templates/archive-bkx_seat.php
+++ templates/archive-bkx_seat.php
@@ -1,30 +1,54 @@
 <?php 
  if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
-$query = new WP_Query( array( 'post_type' => 'bkx_seat') );
 get_header('bkx_seat');
-?>
-<div class="bookingx-lists container">
-		<?php if ( $query->have_posts() ) : ?>
-            <div class="row">
-			<?php
-			// Start the Loop.
-			while ( $query->have_posts() ) : $query->the_post();
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				require 'content-bkx_seat.php';
-			// End the loop.
-			endwhile;?>
-            </div>
-            <?php
+/**
+ * bookingx_before_main_content hook.
+ *
+ * @hooked bookingx_output_content_wrapper - 10 (outputs opening divs for the content)
+ * @hooked bookingx_breadcrumb - 20
+ */
+do_action( 'bookingx_before_main_content' ); ?>
+<div class="booking-x-bkx-seat container">
+    <?php if ( apply_filters( 'booking_x_show_page_title', false ) ) : ?>
+        <h1 class="booking-x-bkx-seats page-title h1"><?php booking_x_page_title(); ?></h1>
+    <?php endif; ?>
 
-		// If no content, include the "No posts found" template.
-		else :
-			do_action('bookingx_no_seats_found');
-		endif;
-		?>
-</div><!-- .Main service listing-area -->
+    <?php if ( have_posts() ) : ?>
+            <div class="row">
+            <?php
+            // Start the Loop.
+            while ( have_posts() ) : the_post();
+                /*
+                 * Include the Post-Format-specific template for the content.
+                 * If you want to override this in a child theme, then include a file
+                 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+                 */
+                require 'content-bkx_seat.php';
+            // End the loop.
+            endwhile;?>
+            </div>
+        <?php
+        // If no content, include the "No posts found" template.
+        else :
+            do_action('booking_x_no_bkx_seat_found');
+        endif;
+    ?>
+</div>
 <?php
-get_footer('bkx_seat');
+    /**
+     * bookingx_after_main_content hook.
+     *
+     * @hooked bookingx_output_content_wrapper_end - 10 (outputs closing divs for the content)
+     */
+    do_action( 'bookingx_after_main_content' );
+?>
+
+<?php
+    /**
+     * bookingx_sidebar hook.
+     *
+     * @hooked bookingx_get_sidebar - 10
+     */
+    do_action( 'bookingx_sidebar' );
+?>
+<?php get_footer('bkx_seat');
