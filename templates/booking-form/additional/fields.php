1691cf2a6118f9e60115c1c9538f3f6bda6e25e1
Divyang Parekh <dparekh4@gmail.com>
2020-05-12 16:14:37 +0530

    Stripe Addon JS Support
    Stripe Addon JS Support

--- templates/booking-form/additional/fields.php
+++ templates/booking-form/additional/fields.php
@@ -5,4 +5,4 @@
 defined( 'ABSPATH' ) || exit;
 ?>
 <input type="hidden" name="bkx_error_flag" id="bkx_error_flag" class="bkx_error_flag" value="0">
-
+<input type="hidden" name="bkx_gateway_flag" id="bkx_gateway_flag" class="bkx_gateway_flag" value="1">
